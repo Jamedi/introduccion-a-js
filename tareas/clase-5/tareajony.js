dd15aa42fa7b4f42e598f36936bdbce216b89654
Jonatan <36902586+Jamedi@users.noreply.github.com>
2020-05-25 17:01:47 -0300

    hecha tarea2 clase-5
    se ha realizado la tarea 2 de la clase 5 maso menos corectamente

--- tareas/clase-5/tareajony.js
+++ tareas/clase-5/tareajony.js
@@ -1,1 +1,20 @@
-let traerNombresApellidos = document.querySelectorAll("input");
+const $botonNombresYapellidos = document.querySelector("#mostrar-nombres");
+
+$botonNombresYapellidos.onclick = function(){
+    const $primerNombre = document.querySelector("#nombre-usuario1").value;
+    const $segundoNombre = document.querySelector("#nombre-usuario2").value;
+    const $primerApellido = document.querySelector("#apellido-usuario1").value;
+    const $segundoApellido = document.querySelector("#apellido-usuario2").value;
+
+    document.querySelector("h1").innerText = `bienvenido ${$primerNombre} ${$segundoNombre} ${$primerApellido} ${$segundoApellido}`;
+    let nombresApellidosUsuarios = document.querySelectorAll("input").value;
+    document.querySelector("#resultado").innerText = `${$primerNombre} ${$segundoNombre} ${$primerApellido} ${$segundoApellido}`;
+    miTitulo = `bienvenido ${$primerNombre} ${$segundoNombre} ${$primerApellido} ${$segundoApellido}`;
+    return false;
+}
+
+
+/*
+let traerNombresApellidos = document.querySelectorAll("imput").value;
+document.querySelector("#resultado").innertext = traerNombresApellidos;
+*/
