3fe359247b0b4b15d245becce9f4085a28bd05b9
Jonatan <36902586+Jamedi@users.noreply.github.com>
2020-05-26 18:15:09 -0300

    preparando para la tarea 3

--- tareas/clase-5/tarea-clase-5.js
+++ tareas/clase-5/tarea-clase-5.js
@@ -34,8 +34,6 @@ $botonCalcular.onclick = function(){
 }
 
 
-
-
 //TAREA2: En otro archivo html (no Index) y otro archivo js (no tarea-clase-5.js),
 // creá un formulario que capture el primer nombre, segundo nombre, apellido/s y edad del usuario
 // también vamos a crear un <h1> que diga Bienvenido!
@@ -53,15 +51,7 @@ Ejemplo form:
 
   <div id="resultado">Hola</div>
 *
-* */
 
-//TAREA3: En otro archivo distinto,
-// Por cada clase de r/argentina programa existente, vamos a pedir:
-// horas, minutos y segundos de cada video. Ej. Si un video dura
-// 2 horas, 38 minutos y 20 segundos, vamos a rellenar 3 campos de texto con
-// cada dato.
-// al apretar el botón "Calcular tiempo total", debe mostrar en un
-// <strong> pre-creado el tiempo total de los videos.
 
 //TAREA4: En otro archivo distinto,
 // Crear una lista de <ol> y <li> que contengan sólo números.
