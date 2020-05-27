a165b8b48c84a45347b83f48a8dd53a32d550ef7
Jonatan <36902586+Jamedi@users.noreply.github.com>
2020-05-27 20:01:53 -0300

    clase 5 Tarea3 terminada
    se ha terminado la tarea 3 de la clase 5

--- tareas/clase-5/tarea3jony.js
+++ tareas/clase-5/tarea3jony.js
@@ -5,3 +5,55 @@
 // cada dato.
 // al apretar el botón "Calcular tiempo total", debe mostrar en un
 // <strong> pre-creado el tiempo total de los videos.
+
+let $segundos=0
+let $minutos=0
+let $horas=0
+
+let $botonHsMemoria = document.querySelectorAll("button");
+
+
+$botonHsMemoria[0].onclick = function (){
+    let usuarioHoras = Number(document.querySelector("#horas").value);
+    let usuarioMinutos = Number(document.querySelector("#minutos").value);
+    let usuarioSegundos = Number(document.querySelector("#segundos").value);
+    
+    $segundos = $segundos + usuarioSegundos;
+    $minutos = $minutos + usuarioMinutos;
+    $horas = $horas + usuarioHoras;
+
+
+    document.querySelector("#segundos").value = ""
+    document.querySelector("#minutos").value = ""
+    document.querySelector("#horas").value = ""
+
+    return false;
+}
+
+function segundos_a_minutos(){
+    let agregarMinutos = Math.floor($segundos/60);
+    $minutos = $minutos + agregarMinutos;
+    $segundos = $segundos % 60;
+}
+
+function minutos_a_horas(){
+    let horas_agregar = Math.floor($minutos/60)
+    $horas = $horas + horas_agregar;
+    $minutos = $minutos % 60;
+  }
+
+  $botonHsMemoria[1].onclick = function(){
+    segundos_a_minutos()
+    minutos_a_horas()
+    if($segundos || $horas || $segundos)
+    {
+      document.querySelector("#total-horas").textContent= `Duracion total de los videos: ${$horas}:${$minutos}:${$segundos}`
+    }
+    else
+    {
+      document.querySelector("#videos-agregados").textContent = "No agregaste nada"
+    }
+    
+    return false;
+  }
+  
