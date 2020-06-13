19cd9f0c682d65472faf0271a27df96e056f2dc6
Jonatan <36902586+Jamedi@users.noreply.github.com>
2020-06-13 14:27:49 -0300

    Tarea 1 clase 6
    se ha realizado la tarea 1 de la clase 6, para ir preparando la tarea2 de la clase 6

--- tareas/clase-6/tarea-clase-6.js
+++ tareas/clase-6/tarea-clase-6.js
@@ -6,6 +6,27 @@ Al hacer click en "calcular", mostrar en un elemento pre-existente la mayor edad
 Punto bonus: Crear un botón para "empezar de nuevo" que empiece el proceso nuevamente, borrando los inputs ya creados (investigar cómo en MDN).
 */
 
+/*
+let CantidadGenteFamilia = Number(prompt("Ingresar cuanto son en la familia?"));
+let nuevoArrayEdad = [];
+
+//let nuevoInputs = document.createElement("input");
+//let nuevoLabels = document.createElement("label");
+//document.querySelector("body").appendChild(nuevoInputs);
+
+for(let i = 1; i <= CantidadGenteFamilia; i++){
+    let nuevoInputs = document.createElement("input");
+    let nuevoLabels = document.createElement("label");
+    document.querySelector("body").appendChild(nuevoLabels);
+    document.querySelector("body").appendChild(nuevoInputs);
+    
+}
+
+let descripcionLabels = document.querySelectorAll("label");
+*/
+
+//console.log(nuevoArrayEdad.length);
+//nuevoArrayEdad = Number(prompt("Edad de cada integrante"));
 
 /*
 TAREA:
